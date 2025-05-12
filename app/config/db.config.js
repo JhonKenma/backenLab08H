5ac6d3dd9956b615d2a7ccfa7f7c793622127ece
Jhons Kenma <jhonskenma@gmail.com>
2025-05-12 00:08:59 -0500

    espero

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -12,10 +12,11 @@ export default {
     acquire: 30000,
     idle: 10000
   },
-  dialectOptions: {
-    ssl: {
-      require: true, // Requiere la conexión SSL
-      rejectUnauthorized: false, // Opcional, deshabilita la verificación del certificado (puedes quitarlo si confías en el certificado)
-    }
+dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
   }
+}
+
 };
